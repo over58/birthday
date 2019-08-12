56850af4ec1ad41f9a45e8a28843fc8bec6eb4e8
yongchao9 <yongchao9@mac-3.local>
2019-08-12 21:20:03 +0800

    update

--- js/index.js
+++ js/index.js
@@ -113,7 +113,7 @@ let counter = 0;
 c2.width = c3.width = window.innerWidth;
 c2.height = c3.height = window.innerHeight;
 ctx1.fillStyle = '#000';
-const text = '奶奶，孙子祝你生日快乐';
+const text = '小芳，生日快乐🦆';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
